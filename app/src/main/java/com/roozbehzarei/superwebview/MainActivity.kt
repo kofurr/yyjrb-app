63d33b263a8c69583bc6d7b8800d807c3466243e
kofurr <63032111@qq.com>
2026-09-17 13:50:56 +0800

    Update website URL in MainActivity

--- app/src/main/java/com/roozbehzarei/superwebview/MainActivity.kt
+++ app/src/main/java/com/roozbehzarei/superwebview/MainActivity.kt
@@ -37,7 +37,7 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.roozbehzarei.superwebview.ui.theme.SuperWebViewTheme
 
-private const val WEBSITE = "https://roozbehzarei.com"
+private const val WEBSITE = "https://www.yyjrb.com.cn"
 
 /**
  * Main activity of the application.
