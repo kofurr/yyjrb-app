c1499bc32a1b10a25fd766e767e48d59fe0b6afb
kofurr <63032111@qq.com>
2026-09-17 17:34:11 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,3 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.compose.compiler) apply false
 }
-
-android {
-    // ... 其他配置 ...
-
-    lint {
-        abortOnError false   // Lint 有错误时不中断构建
-        checkReleaseBuilds false  // Release 构建时不跑 Lint
-    }
-}
